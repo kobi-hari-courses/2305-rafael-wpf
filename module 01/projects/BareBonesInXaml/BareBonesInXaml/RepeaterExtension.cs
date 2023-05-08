e451ed6f0eff661d269f92553f26352c5566486a
Kobi Hari <kobi2294@yahoo.com>
2023-05-08 17:00:19 +0300

    Fun with Templates - Part 1

---
+++ module 01/projects/BareBonesInXaml/BareBonesInXaml/RepeaterExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace BareBonesInXaml
+{
+    public class RepeaterExtension : MarkupExtension
+    {
+        public string Text { get; set; } = "*";
+
+        public int Times { get; set; } = 3;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < Times; i++)
+            {
+                sb.Append(Text);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
