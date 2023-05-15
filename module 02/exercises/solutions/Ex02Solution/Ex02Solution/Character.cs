dd98766436c1205b7c503639c510013c41791f52
Kobi Hari <kobi2294@yahoo.com>
2023-05-15 14:57:47 +0300

    Ex 2 solution

---
+++ module 02/exercises/solutions/Ex02Solution/Ex02Solution/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02Solution
+{
+    public class Character
+    {
+        public string Text { get; set; } = "";
+
+        public string FontName { get; set; } = "";
+
+        public string FontSize { get; set; } = "";
+    }
+}
---
+++ module 02/exercises/solutions/Ex02Solution/Ex02Solution/Rgb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02Solution
+{
+    public class Rgb
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+    }
+}
