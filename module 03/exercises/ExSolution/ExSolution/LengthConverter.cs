ad3bc2439acf0a334675176a36c186fbae650381
Kobi Hari <kobi2294@yahoo.com>
2023-06-01 16:10:56 +0300

    solution for example

---
+++ module 03/exercises/ExSolution/ExSolution/LengthConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ExSolution
+{
+    public class LengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var length = (TimeSpan)value;
+
+                var hours = length.Hours;
+                var minutes = length.Minutes;
+
+                if ((hours == 0) && (minutes == 30)) return "Half hour";
+                if (minutes == 30) return $"{hours} hours and a half";
+                if (hours == 0) return $"{minutes} minutes";
+                if (minutes == 0) return $"{hours} hours";
+
+                return $"{hours} hours and {minutes} minutes";
+
+            }
+            catch { }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ module 03/exercises/ExSolution/ExSolution/Meeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExSolution
+{
+    public class Meeting
+    {
+        public string Title { get; set; } = "";
+
+        public DateTime Start { get; set; } = DateTime.Now;
+
+        public TimeSpan Length { get; set; } = TimeSpan.Zero;
+    }
+}
