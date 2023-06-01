ec07747612414a5e38e3a33da7e69467a63e0185
Kobi Hari <kobi2294@yahoo.com>
2023-06-01 11:33:18 +0300

    Dependency Properties

---
+++ module 03/projects/FunWithDProperties/FunWithDProperties/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FunWithDProperties
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var c = new CustomObject();
+            var valueOfA = c.A;
+            var source = DependencyPropertyHelper.GetValueSource(c, CustomObject.AProperty);
+
+            var style = new Style(typeof(CustomObject));
+            style.Setters.Add(new Setter(CustomObject.AProperty, 100));
+            c.Style = style;
+            valueOfA = c.A;
+            source = DependencyPropertyHelper.GetValueSource(c, CustomObject.AProperty);
+
+            c.Max = 80;
+            valueOfA = c.A;
+            source = DependencyPropertyHelper.GetValueSource(c, CustomObject.AProperty);
+
+            c.A = 50;
+            valueOfA = c.A;
+            source = DependencyPropertyHelper.GetValueSource(c, CustomObject.AProperty);
+
+            c.ClearValue(CustomObject.AProperty);
+            valueOfA = c.A;
+            source = DependencyPropertyHelper.GetValueSource(c, CustomObject.AProperty);
+
+            c.Style = null;
+            valueOfA = c.A;
+            source = DependencyPropertyHelper.GetValueSource(c, CustomObject.AProperty);
+
+
+            c.SetValue(Button.BackgroundProperty, Brushes.Green);
+
+            var colorOfC = c.GetValue(Button.BackgroundProperty);
+
+
+        }
+    }
+}
---
+++ module 03/projects/FunWithDProperties/FunWithDProperties/CustomObject.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FunWithDProperties
+{
+    public class CustomObject: FrameworkElement
+    {
+        public static readonly DependencyProperty AProperty = DependencyProperty.Register(
+            nameof(A), typeof(int), typeof(CustomObject), new PropertyMetadata(42, OnAChanged, OnACoerced)
+            );
+
+        public static readonly DependencyProperty MaxProperty = DependencyProperty.Register(
+            nameof(Max), typeof(int), typeof(CustomObject), new PropertyMetadata(90, OnMaxChanged)
+            );
+
+        private static readonly DependencyPropertyKey BPropertyKey = DependencyProperty.RegisterReadOnly(
+            "B", typeof(int), typeof(CustomObject), new PropertyMetadata(52));
+
+        public static readonly DependencyProperty BProperty = BPropertyKey.DependencyProperty;
+
+        private static void OnMaxChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.InvalidateProperty(AProperty);
+        }
+
+        private static object OnACoerced(DependencyObject d, object baseValue)
+        {
+            var customObject = (d as CustomObject)!;
+            var value = (int)baseValue;
+            if (value > customObject.Max) return customObject.Max;
+
+            return value;
+        }
+
+        private static void OnAChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Debug.WriteLine($"Value A changed from {e.OldValue} to {e.NewValue} ");
+            (d as CustomObject)!.B = (int)e.NewValue + 10;
+        }
+
+        public int Max
+        {
+            get
+            {
+                return (int)GetValue(MaxProperty);
+            }
+            set 
+            { 
+                SetValue(MaxProperty, value); 
+            }
+        }
+
+        public int A
+        {
+            get
+            {
+                return (int)GetValue(AProperty);
+            }
+            set
+            {
+                SetValue(AProperty, value);
+            }
+        }
+
+        public int B
+        {
+            get
+            {
+                return (int)GetValue(BProperty);
+            }
+            private set
+            {
+                SetValue(BPropertyKey, value);
+            }
+        }
+
+    }
+}
